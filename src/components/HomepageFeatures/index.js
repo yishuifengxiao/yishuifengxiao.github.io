c99a5c126ffa15edc16a908b6ae435499ce0b308
yishuifengxiao <zhiyubujian@163.com>
2023-02-16 08:49:24 +0800

    修改首页

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,43 +1,32 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import styles from "./styles.module.css";
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: "开箱即用",
+    Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
+    description: <>配置异常简单，适合菜鸟入门使用。</>,
+  },
+  {
+    title: "配置灵活",
+    Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        无须二次配置即能使用组件的基本工功能.能通过预留的配置属性自定义组件功能。
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
+    title: "功能简洁",
+    Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
+    description: <>增强扩展，不影响原生功能.</>,
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({ Svg, title, description }) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
@@ -57,6 +46,9 @@ export default function HomepageFeatures() {
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
           ))}
+        </div>
+        <div className={styles.tip}>
+          <p> 午静携侣觅野菜</p> <p> 黄昏抱猫向斜阳</p> <p> 当时只道是寻常</p>
         </div>
       </div>
     </section>
